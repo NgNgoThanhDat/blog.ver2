acdc5529ec778c26bea2b0a4d9f0781e7fb1248a
Dash Nguyen <datthanh.9982@gmail.com>
2016-10-06 09:25:25 +0700

    [Dat] - login & logout by username, email

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
   resources :pages
   resources :users
-  post '/signup', to: 'users#create'
+  
   get  '/signup',  to: 'users#new'
   root 'myapp#index'
 
