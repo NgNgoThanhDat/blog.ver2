60c12dd5ffd95b58eeb63f7e0bb7b494e5f74910
Le Hong Cuong <1312061@student.hcmus.edu.vn>
2016-10-08 11:22:23 +0700

    [Cuong] - Update - Edit SCSS 1

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 User.create!(name: "ThanhDat", email: "datthanh.9982@gmail.com", password: "Dell5537", password_confirmation: "Dell5537", admin: true)
 
-99.times do |n|
+9.times do |n|
   name  = Faker::Name.name
   email = "example-#{n+1}@gmail.com"
   password = "password"
@@ -16,7 +16,7 @@ end
 
 # Posts
 users = User.order(:created_at).take(6)
-50.times do
+5.times do
   content = Faker::Lorem.sentence(5)
   users.each { |user| user.microposts.create!(content: content) }
 end
@@ -24,7 +24,7 @@ end
 # Following relationships
 users = User.all
 user  = users.first
-following = users[2..50]
-followers = users[3..40]
+following = users[2..5]
+followers = users[3..8]
 following.each { |followed| user.follow(followed) }
 followers.each { |follower| follower.follow(user) }
